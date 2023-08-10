0350b30422cf56bfcbf74b0719dd64e2573edd81
JuanCaceffo <caceffojuan@gmail.com>
2023-08-10 11:19:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <headering>
+        <h1>Conversor millas a km</h1>
+      </headering>
+      <form>
+        <label>Millas</label>
+        <input autoComplete='off'></input>
+      </form>
     </div>
   );
 }
